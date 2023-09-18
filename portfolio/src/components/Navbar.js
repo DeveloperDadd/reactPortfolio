d1b628a158abe99223599f72f2b45a87f3e1b8bd
claytoncritchfield <clayton.critchfield@gmail.com>
2023-09-18 19:55:19 -0400

    added some simple styling and responsiveness to the nav, will add more later

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,11 +1,11 @@
 export default function Navbar() {
     return(
     <nav className="container-fluid d-flex justify-content-center">
-        <ul className="d-flex list-unstyled .flex-row">
-            <li className="m-5"><a>About Me</a></li>
-            <li className="m-5"><a>Skills</a></li>
-            <li className="m-5"><a>Projects</a></li>
-            <li className="m-5"><a>Contact Me</a></li>
+        <ul className="d-flex list-unstyled .flex-row row">
+            <li className="m-5 col-sm"><a href="#">About Me</a></li>
+            <li className="m-5 col-sm"><a href="#">Skills</a></li>
+            <li className="m-5 col-sm"><a href="#">Projects</a></li>
+            <li className="m-5 col-sm"><a href="#">Contact Me</a></li>
         </ul>
     </nav>)
 }
