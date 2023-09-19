19be70b8887ff0a98dc4f65a855d74aede9d1ce3
claytoncritchfield <clayton.critchfield@gmail.com>
2023-09-18 21:18:33 -0400

    saving before logging off for the night

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -1,18 +1,21 @@
 export default function Skills() {
     return (
-        <section className="text-center">
-            <h2>My Current Tech Stack</h2>
-            <div className="row">
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/html.png" /></div>
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/css3.png" /></div>
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/javascript.png" /></div>
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/python.png" /></div>
+        <section className="text-center m-5">
+            <h1 className="mb-5">My Current Tech Stack</h1>
+            <div className="row mb-5">
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/html.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/css3.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/javascript.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/python.png" /></div>
             </div>
             <div className="row">
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/react.png" /></div>
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/rest.png" /></div>
-                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/django.jpg" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/react.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/rest.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/django.png" /></div>
+                <div className="col-3"><img className="img-fluid tech-image" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/PostgreSQL.png" /></div>
             </div>
+            <h1 className="mt-5">Tech Stack I am working on learning:</h1>
+            <p>While I do appreciate Python and its ease of use. I am planning to learn these technologies to keep everything JavaScript related. I would eventually like to come back to Python to learno more about machine learning. </p>
         </section>
     )
 }
