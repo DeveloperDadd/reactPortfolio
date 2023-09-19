2c0c04c1bac6ef083c2165fc5ef14fd0c1fcb78c
claytoncritchfield <clayton.critchfield@gmail.com>
2023-09-18 20:46:47 -0400

    started adding tech stack images

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -2,6 +2,17 @@ export default function Skills() {
     return (
         <section className="text-center">
             <h2>My Current Tech Stack</h2>
+            <div className="row">
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/html.png" /></div>
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/css3.png" /></div>
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/javascript.png" /></div>
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/python.png" /></div>
+            </div>
+            <div className="row">
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/react.png" /></div>
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/rest.png" /></div>
+                <div className="col-3"><img className="img-fluid" src="https://raw.githubusercontent.com/DeveloperDadd/reactPortfolio/main/portfolio/src/img/django.jpg" /></div>
+            </div>
         </section>
     )
 }
