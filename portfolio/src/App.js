cb2448fd1f83f6d61941e69cf3fb249e7d565fba
claytoncritchfield <clayton.critchfield@gmail.com>
2023-09-18 20:17:28 -0400

    created about me section, added some imagery and a small about me section will style more later

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar';
-
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AboutMe />
     </div>
   );
 }
