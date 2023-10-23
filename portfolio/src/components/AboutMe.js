2a84aeba0367d00f8132bc9c31a68f18566326b7
claytoncritchfield <clayton.critchfield@gmail.com>
2023-10-22 21:22:16 -0400

    Add CSS styling, modified font-sizes to be more responsive

--- portfolio/src/components/AboutMe.js
+++ portfolio/src/components/AboutMe.js
@@ -1,10 +1,10 @@
 export default function AboutMe() {
     return(
-        <section className="container-fluid p-5">
+        <section className="about-me container-fluid p-5">
             <div className="row d-flex justify-content-center align-items-center">
                 <div className="col-md-6">
-                    <h1 className="text-center">Hi! I'm Clayton, a Fullstack Developer</h1>
-                    <p className="text-center mt-3">I recently completed a fulltime web development bootcamp hosted by Awesome Inc in August of 2023. We covered everything from HTML to Django Backend and REST APIs. Since then I have been working on going deeper into the foundation they laid to become a better fullstack developer. I will continue to add my projects to my portfolio here as I continue to grow! Aside from coding, I'm a father to a beautiful little girl, I also love to read, play video games, and binge watch 'The Office' with my wife. Thanks for checking out my portfolio, I hope to connect!</p>
+                    <h1 className="text-center">Hi! I'm Clayton,<br></br> a Fullstack Developer</h1>
+                    <p className="about-para text-center mt-3">I recently completed a fulltime web development bootcamp hosted by Awesome Inc in August of 2023. We covered everything from HTML to Django Backend and REST APIs. Since then I have been working on going deeper into the foundation they laid to become a better fullstack developer. I will continue to add my projects to my portfolio here as I continue to grow! Aside from coding, I'm a father to a beautiful little girl, I also love to read, play video games, and binge watch 'The Office' with my wife. Thanks for checking out my portfolio, I hope to connect!</p>
                 </div>
             </div>
         </section>
